468c2d61aefa71bcf30687eb284931d142527515
Gusberly <gusberlymorillo@gmail.com>
2022-12-15 14:15:08 -0300

    actualizando

--- src/Components/LandingPage/Landing.jsx
+++ src/Components/LandingPage/Landing.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Styles from "./Landing.module.css";
+import landing from "../../Imagenes/landing.jpg";
 
 export default function Landing() {
   return (
-    <div className={Styles.Landing}>
+    <div>
+      <img src={landing} alt="landing" />
+      <div></div>
       <Link to="/home">
         <button className={Styles.btn}>COMENZAR</button>
       </Link>
